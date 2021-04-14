84b56da2b01ce7f70b70471f7cec7e95a5bbcf21
ZSJ <1940982510@qq.com>
2021-04-14 20:06:02 +0800

    账户余额

--- src/main/java/com/sdu/broker/controller/StorageController.java
+++ src/main/java/com/sdu/broker/controller/StorageController.java
@@ -3,10 +3,7 @@ package com.sdu.broker.controller;
 import com.sdu.broker.pojo.StorageType;
 import com.sdu.broker.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +13,13 @@ public class StorageController {
     private StorageService storageService;
 
     @CrossOrigin
-    @RequestMapping(value = "/getAli", method = RequestMethod.GET)
+    @GetMapping(value = "/getAli")
     public List<StorageType> getAli() {
         return storageService.getAli();
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/getHuawei", method = RequestMethod.GET)
+    @GetMapping(value = "/getHuawei")
     public List<StorageType> getHuawei() {
         return storageService.getHuawei();
     }
