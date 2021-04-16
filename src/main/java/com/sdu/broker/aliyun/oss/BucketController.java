8701b23b85bfcd3c0d349bb3c2e2b067527fd31d
jiaxuyang <909091385@qq.com>
2021-04-16 22:16:35 +0800

    aliyun 创建存储空间(存储类型、冗余类型、读写控制）

--- src/main/java/com/sdu/broker/aliyun/oss/BucketController.java
+++ src/main/java/com/sdu/broker/aliyun/oss/BucketController.java
@@ -4,8 +4,8 @@ import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
-import com.aliyun.oss.model.BucketInfo;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.aliyun.oss.model.*;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
 
 //@ConfigurationProperties(prefix = 'ali')
 public class BucketController {
@@ -13,8 +13,7 @@ public class BucketController {
     private static String accessKeyId = "LTAI5tE3U2xuvubTk8qocyd2";
     private static String accessKeySecret = "Q0cqcMmjKGBmyRM6s0G51QYCMSn6aO";
 
-    public static void  createBucket(String bucketName){
-
+    public static void  createBucket(String bucketName, int storageClass, int dataRedundancyType, int cannedACL){
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
         try{
@@ -22,7 +21,41 @@ public class BucketController {
                 System.out.println("您已创建Bucket:" + bucketName + "。");
             }else {
                 System.out.println("您的Bucket不存在，创建Bucket:" + bucketName + "。");
-                ossClient.createBucket(bucketName);
+
+                CreateBucketRequest bucketRequest = new CreateBucketRequest(bucketName);
+
+                switch (storageClass){
+                    case 1:
+                        bucketRequest.setStorageClass(StorageClass.Standard);
+                        break;
+                    case 2:
+                        bucketRequest.setStorageClass(StorageClass.IA);
+                        break;
+                    case 3:
+                        bucketRequest.setStorageClass(StorageClass.Archive);
+                    case 4:
+                        bucketRequest.setStorageClass(StorageClass.ColdArchive);
+                }
+
+                switch (dataRedundancyType){
+                    case 0:
+                        bucketRequest.setDataRedundancyType(DataRedundancyType.LRS);
+                        break;
+                    case 1:
+                        bucketRequest.setDataRedundancyType(DataRedundancyType.ZRS);
+                        break;
+                }
+
+                switch (cannedACL){
+                    case 1:
+                        bucketRequest.setCannedACL(CannedAccessControlList.Private);
+                        break;
+                    case 2:
+                        bucketRequest.setCannedACL(CannedAccessControlList.PublicRead);
+                    case 3:
+                        bucketRequest.setCannedACL(CannedAccessControlList.PublicReadWrite);
+                }
+                ossClient.createBucket(bucketRequest);
             }
 
             BucketInfo info = ossClient.getBucketInfo(bucketName);
@@ -39,11 +72,13 @@ public class BucketController {
         } finally{
             ossClient.shutdown();
         }
+
+
     }
 
     public static void main(String[] args) {
-        String bucket_name = "xmsx-001";
-        createBucket(bucket_name);
+        String bucket_name = "xmsx-002";
+        createBucket(bucket_name,1,1,2);
     }
 
 }
