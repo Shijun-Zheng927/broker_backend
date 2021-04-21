736924b82280c0a37298118a5231b36b2553ac68
ZSJ <1940982510@qq.com>
2021-04-21 13:48:30 +0800

    修改字段

--- src/main/java/com/sdu/broker/mapper/StorageMapper.java
+++ src/main/java/com/sdu/broker/mapper/StorageMapper.java
@@ -25,13 +25,14 @@ public interface StorageMapper {
     List<StorageType> getAli();
 
     @Select("select platform, type, scene, introduction, min_storage_time, design_persistence_single, design_availability_single, " +
-            "design_persistence_multi, design_availability_multi, huawei_price from storage_type where platform = 'HUAWEI'")
+            "design_persistence_multi, design_availability_multi, response_time, huawei_price from storage_type where platform = 'HUAWEI'")
     @Results({
             @Result(property = "minStorageTime", column = "min_storage_time"),
             @Result(property = "designPersistenceSingle", column = "design_persistence_single"),
             @Result(property = "designAvailabilitySingle", column = "design_availability_single"),
             @Result(property = "designPersistenceMulti", column = "design_persistence_multi"),
             @Result(property = "designAvailabilityMulti", column = "design_availability_multi"),
+            @Result(property = "responseTime", column = "response_time"),
             @Result(property = "huaweiPrice", column = "huawei_price")
     })
     List<StorageType> getHuawei();
