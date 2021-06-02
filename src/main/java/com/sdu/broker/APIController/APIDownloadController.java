ef8ae4df371117863a82a55707169db6f47d1de7
ZSJ <1940982510@qq.com>
2021-06-02 10:23:58 +0800

    demo

---
+++ src/main/java/com/sdu/broker/APIController/APIDownloadController.java
@@ -0,0 +1,83 @@
+package com.sdu.broker.APIController;
+
+import com.sdu.broker.service.BucketService;
+import com.sdu.broker.utils.TokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Objects;
+
+@Controller
+@CrossOrigin
+public class APIDownloadController {
+    @Autowired
+    private BucketService bucketService;
+
+    @ResponseBody
+    @RequestMapping(value = "/demo", method = RequestMethod.POST)
+    public String demo(@RequestBody Map<String, String> map,
+                               @RequestHeader("Authorization") String authorization, HttpServletResponse response) {
+        if (!verifyIdentity(response, authorization)) {
+            return null;
+        }
+        Integer userId = Integer.valueOf(Objects.requireNonNull(TokenUtils.getUserId(authorization)));
+        String bucketName = map.get("bucketName");
+        if (verify(response, userId, bucketName)) {
+            return null;
+        }
+        String platform = bucketService.getPlatform(bucketName);
+        if (platform.equals("ALI")) {
+            //在此获取其他参数并验证
+            String acl = map.get("rwPolicy");
+            if ("".equals(acl)) {
+                //设置默认值
+                acl = "0";
+            }
+
+            //阿里云在此调用方法
+//            String result = bucketController.setBucketAcl(bucketName, Integer.parseInt(acl));
+
+            //返回结果
+            return "result";
+        } else {
+            String rwPolicy = map.get("rwPolicy");
+            if ("".equals(rwPolicy)) {
+                //设置默认值
+                rwPolicy = "0";
+            }
+
+            //华为云在此进行方法调用
+//            huaweiController.setBucketAcl(bucketName, Integer.parseInt(rwPolicy));
+
+            //返回结果
+            return "result";
+        }
+    }
+
+
+    //工具方法
+    public boolean verify(HttpServletResponse response, Integer userId, String bucketName) {
+        if ("".equals(bucketName)) {
+            response.setStatus(777);
+            return true;
+        }
+//        Bucket bucket = new Bucket(userId, platform, bucketName);
+        Integer legal = bucketService.verify(userId.toString(), bucketName);
+        if (legal == null) {
+            response.setStatus(666);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verifyIdentity(HttpServletResponse response, String token) {
+        if (!TokenUtils.verify(token)) {
+            response.setStatus(999);
+            return false;
+        }
+        return true;
+    }
+}
